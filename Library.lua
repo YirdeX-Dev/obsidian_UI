2f5f8c83035133d4c9ae53e23a1fce0717da2928
YirdeX-Dev <3939691930@qq.com>
2026-09-24 21:51:03 +0800

    更新 Library.lua

--- Library.lua
+++ Library.lua
@@ -274,18 +274,18 @@ local Library = {
 
     --// Scheme \\--
     IsLightTheme = false,
-    Scheme = {
-        BackgroundColor = Color3.fromRGB(15, 15, 15),
-        MainColor = Color3.fromRGB(25, 25, 25),
-        AccentColor = Color3.fromRGB(125, 85, 255),
-        OutlineColor = Color3.fromRGB(40, 40, 40),
-        FontColor = Color3.new(1, 1, 1),
-        Font = Font.fromEnum(Enum.Font.Code),
+Scheme = {
+    BackgroundColor = Color3.fromRGB(15, 15, 15),
+    MainColor = Color3.fromRGB(25, 25, 25),
+    AccentColor = Color3.fromRGB(0, 255, 255),
+    OutlineColor = Color3.fromRGB(40, 40, 40),
+    FontColor = Color3.fromRGB(255, 255, 255),
+    Font = Font.fromEnum(Enum.Font.Code),
 
-        RedColor = Color3.fromRGB(255, 50, 50),
-        DestructiveColor = Color3.fromRGB(220, 38, 38),
-        DarkColor = Color3.new(0, 0, 0),
-        WhiteColor = Color3.new(1, 1, 1),
+    RedColor = Color3.fromRGB(255, 50, 50),
+    DestructiveColor = Color3.fromRGB(220, 38, 38),
+    DarkColor = Color3.new(0, 0, 0),
+    WhiteColor = Color3.new(1, 1, 1),
 
         BackgroundImage = ""
     },
